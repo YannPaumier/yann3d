250967b5576cd91bd615a255257ba747481d1aaa
yannpaumier <ypaumier@belacom.fr>
2018-05-13 22:41:23 +0200

    little fix

--- src/client/js/spectator.js
+++ src/client/js/spectator.js
@@ -1,10 +1,10 @@
 Spectator = function(game){
-
-}
+ this._initCamera(game);
+};
 
 Spectator.prototype = {
-  _initCamera = () => {
-    this.camera =  new BABYLON.ArcRotateCamera("Camera", 40, 20, 50, new BABYLON.Vector3(0, 0, 1), scene);
+  _initCamera : function(game) {
+    this.camera =  new BABYLON.ArcRotateCamera("Camera", 40, 20, 50, new BABYLON.Vector3(0, 0, 1), game.scene);
     this.camera.position = BABYLON.Vector3(0, 1, 0);
 
   }
